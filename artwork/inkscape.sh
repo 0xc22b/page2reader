5fbd485b3960471017e43b213b2882e43fbe719a
witterk <witterk@gmail.com>
2013-07-20 14:57:21 +0700

    gen more png files for Android

--- artwork/inkscape.sh
+++ artwork/inkscape.sh
@@ -10,7 +10,9 @@ path="/Users/wit/Workspace/Misc/page2reader/artwork"
 /Applications/Inkscape.app/Contents/Resources/bin/inkscape $path/icon.svg --export-png=$path/icon-64x64.png -w64 -h64
 /Applications/Inkscape.app/Contents/Resources/bin/inkscape $path/icon.svg --export-png=$path/icon-128x128.png -w128 -h128
 
+/Applications/Inkscape.app/Contents/Resources/bin/inkscape $path/logo.svg --export-png=$path/logo-48x48.png -w48 -h48
 /Applications/Inkscape.app/Contents/Resources/bin/inkscape $path/logo.svg --export-png=$path/logo-57x57.png -w57 -h57
 /Applications/Inkscape.app/Contents/Resources/bin/inkscape $path/logo.svg --export-png=$path/logo-72x72.png -w72 -h72
+/Applications/Inkscape.app/Contents/Resources/bin/inkscape $path/logo.svg --export-png=$path/logo-96x96.png -w96 -h96
 /Applications/Inkscape.app/Contents/Resources/bin/inkscape $path/logo.svg --export-png=$path/logo-114x114.png -w114 -h114
 /Applications/Inkscape.app/Contents/Resources/bin/inkscape $path/logo.svg --export-png=$path/logo-144x144.png -w144 -h144
