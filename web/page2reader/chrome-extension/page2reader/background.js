b5a449650fe1cb5d2e7eeb2b64f38ab0b1b7bf68
witterk <witterk@gmail.com>
2013-07-22 22:25:24 +0700

    Chrome extension: inserted and called the script locally for security reasons

--- web/page2reader/chrome-extension/page2reader/background.js
+++ web/page2reader/chrome-extension/page2reader/background.js
@@ -1,11 +1,3 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-  var actionUrl = 'javascript:((function() {' +
-      'var baseUrl = \'http://page2reader.appspot.com\';' +
-      'var s = document.createElement(\'script\');' +
-      's.setAttribute(\'type\', \'text/javascript\');' +
-      's.setAttribute(\'charset\', \'UTF-8\');' +
-      's.setAttribute(\'src\', baseUrl + \'/bookmarklet/sendp2r.js\');' +
-      'document.documentElement.appendChild(s);' +
-      '})());';
-  chrome.tabs.update(tab.id, {url: actionUrl});
+  chrome.tabs.executeScript(tab.id, {file: 'sendp2r.js'});
 });
